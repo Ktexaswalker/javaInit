6c3021580074c03ac9ecbba6a0d78ef0f52d5ce4
Hector <hector.mc1988@gmail.com>
2023-10-03 20:02:05 +0200

    Ifs terminados

--- UltimosIfs1/src/ultimosifs1/UltimosIfs1.java
+++ UltimosIfs1/src/ultimosifs1/UltimosIfs1.java
@@ -4,9 +4,14 @@
  * and open the template in the editor.
  */
 package ultimosifs1;
+import java.util.Scanner;
 
-/**
- *
+/**1
+ * Calcular l’augment de sou al cap d’any a partir d’aquesta taula
+ * inferior 18000 -> 12%
+ * entre 18000 y 30000 -> 10%
+ * superior a 30000 e inferior a 45000 8%
+ * 45000 o mas -> 6%
  * @author hecto
  */
 public class UltimosIfs1 {
@@ -15,7 +20,26 @@ public class UltimosIfs1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduzca su sueldo: ");
+        double sueldo = sc.nextInt();
+        double futuro_sueldo; int porcentaje; int diferencia;
+        if (sueldo < 18000) {
+            futuro_sueldo = sueldo * 1.12;
+            porcentaje = 12;
+        } else if (sueldo >= 18000 && sueldo <= 30000) {
+            futuro_sueldo = sueldo * 1.10;
+            porcentaje = 10;
+        } else if (sueldo > 30000 && sueldo < 45000) {
+            futuro_sueldo = sueldo * 1.08;
+            porcentaje = 8;
+        } else {
+            futuro_sueldo = sueldo * 1.06;
+            porcentaje = 6;
+        }
+        diferencia = (int) (futuro_sueldo - sueldo);
+        System.out.println("Tu sueldo aumentara un: " + porcentaje + "%, que equivale a: " + diferencia + " Euros.");
+        System.out.println("Tu futuro sueldo el año que viene sera de: " + futuro_sueldo);
     }
     
 }
---
+++ UltimosIfs2/src/ultimosifs2/UltimosIfs2.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ultimosifs2;
+import java.util.Scanner;
+/**
+ *
+ * @author hecto
+ */
+public class UltimosIfs2 {
+
+    /**2
+     * Descuento en supermercado
+     * inferior a 800 -> 0%
+     * entre 800 y 1500 -> 6%
+     * superior a 1500 y hasta 3000 -> 8%
+     * mas de 3000 -> 10%
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Cuanto vas a pagar en el supermercado? ");
+        int preu = sc.nextInt(); float descompte; float precio_final;
+        if (preu < 800) {
+            System.out.println("Recibes un descuento del 0%");
+            descompte = 0;
+        } else if (preu >= 800 && preu <= 1500) {
+            System.out.println("Recibes un descuento del 6%");
+            descompte = 6;
+        } else if (preu > 1500 && preu <= 3000) {
+            System.out.println("Recibes un descuento del 8%");
+            descompte = 8;
+        } else {
+            System.out.println("Recibes un descuento del 10%");
+            descompte = 10;
+        }
+        precio_final = (preu*descompte)/100;
+        System.out.println("Con el " + descompte + "% de descuento has ahorrado: " + precio_final + " Euros");
+        preu = (int) (preu - precio_final);
+        System.out.println("Esto supone en tu factura un precio final de: " + preu + " Euros");
+    }
+    
+}
